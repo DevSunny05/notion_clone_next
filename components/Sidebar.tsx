1dceb5fc785f99135fac0ddca494c2388a527861
sarang <sarang.office01@gmail.com>
2024-11-12 17:11:30 +0530

    test

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,7 +3,7 @@ import NewDocumentButton from './NewDocumentButton'
 
 const Sidebar = () => {
   return (
-    <div className='p-2 md:p-5'>
+    <div className='p-2 md:p-5 '>
         <NewDocumentButton/>
     </div>
   )
