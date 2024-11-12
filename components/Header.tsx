26dadadc11470b05b80c99e53b368af27a6c500c
sarang <sarang.office.01@gmail.com>
2024-11-12 17:07:41 +0530

    header created

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/clerk-react'
+import { useUser } from '@clerk/nextjs'
+import React from 'react'
+
+const Header = () => {
+    const {user}=useUser()
+  return (
+    <div className='flex items-center justify-between p-5'>
+        {
+            user && (
+                <h1 className='text-2xl'>
+                    {user?.firstName}
+                    {`'s`}
+                </h1>
+            )
+        }
+
+        {/* breadcrum */}
+
+        <SignedOut>
+            <SignInButton/>
+        </SignedOut>
+
+        <SignedIn>
+            <UserButton/>
+        </SignedIn>
+
+    </div>
+  )
+}
+
+export default Header
